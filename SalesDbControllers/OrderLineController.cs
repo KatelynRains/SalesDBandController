bdafe06b022640f284790ca90dc5bc1ce9b70da0
Katelyn Rains <KRains@tql.com>
2021-05-28 13:56:37 -0400

    adding finished customers and started order lines

---
+++ SalesDbControllers/OrderLineController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesDbControllers
+{
+    class OrderLineController
+    {   private static Connection connection { get; set; }
+
+        //constructor to bring instance into class
+        public OrderLineController(Connection connection)
+        { OrderLineController.connection = connection; }
+
+        
+
+
+
+
+    }
+}

95d1845b10b27f59f2ad220d96ae44b57a01a8ba
Sarah George <sarahgeorge1394@gmail.com>
2021-05-28 13:53:38 -0400

    Added order class and order controller class

---
+++ SalesDbControllers/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesDbControllers
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
